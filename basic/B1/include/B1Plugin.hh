2155dd299d7bc6bdf3d865d8bd4ffa84784bc336
Jan Pipek <jan.pipek@gmail.com>
2015-07-17 22:50:33 +0200

    B1 compilable

--- basic/B1/include/B1Plugin.hh
+++ basic/B1/include/B1Plugin.hh
@@ -5,20 +5,41 @@
 #include "util/ThreadLocal.hh"
 
 #include "B1EventAction.hh"
+#include "B1PrimaryGeneratorAction.hh"
+
+class B1DetectorConstruction;
 
 class B1Plugin : public g4::SingleComponentPlugin
 {
 public:
+    B1Plugin();
+
+    virtual ~B1Plugin();
+
     virtual void BuildGeometry(G4LogicalVolume *logVolume) override;
+
+    virtual G4VPhysicalVolume* CreateWorld() override;
 
     virtual G4UserRunAction* CreateRunAction() override;
 
     virtual G4UserEventAction* CreateEventAction() override;
 
+    virtual G4UserSteppingAction* CreateSteppingAction() override;
+
     virtual G4VUserPrimaryGeneratorAction* CreatePrimaryGeneratorAction() override;
 
+    // To be used in the plugin
+    B1PrimaryGeneratorAction* GetPrimaryGeneratorAction() { return _primaryGeneratorAction.Get(); }
+
+    B1DetectorConstruction* GetDetectorConstruction() { return _detectorConstruction; }
+
+    B1EventAction* GetEventAction(); // Lazy-evaluated, creates on demand
+
 private:
-    B1EventAction* GetEventAction();
+
+    B1DetectorConstruction* _detectorConstruction;
+
+    g4::util::ThreadLocal<B1PrimaryGeneratorAction> _primaryGeneratorAction;
 
     g4::util::ThreadLocal<B1EventAction> _eventAction;
 };
--- basic/B1/src/B1Plugin.cc
+++ basic/B1/src/B1Plugin.cc
@@ -1,15 +1,33 @@
 #include "B1Plugin.hh"
 
+#include "B1RunAction.hh"
 #include "B1EventAction.hh"
+#include "B1SteppingAction.hh"
+#include "B1PrimaryGeneratorAction.hh"
+#include "B1DetectorConstruction.hh"
+
+B1Plugin::B1Plugin()
+    : _detectorConstruction(new B1DetectorConstruction)
+{ }
+
+B1Plugin::~B1Plugin()
+{
+    delete _detectorConstruction;
+}
+
+G4VPhysicalVolume* B1Plugin::CreateWorld()
+{
+    return _detectorConstruction->CreateWorld();
+}
 
 void B1Plugin::BuildGeometry(G4LogicalVolume *logVolume)
 {
-
+    _detectorConstruction->BuildGeometry(logVolume);
 }
 
 G4UserRunAction* B1Plugin::CreateRunAction()
 {
-
+    return new B1RunAction(this);
 }
 
 G4UserEventAction* B1Plugin::CreateEventAction()
@@ -17,9 +35,15 @@ G4UserEventAction* B1Plugin::CreateEventAction()
     return GetEventAction();
 }
 
+G4UserSteppingAction* B1Plugin::CreateSteppingAction()
+{
+    return new B1SteppingAction(this);
+}
+
 G4VUserPrimaryGeneratorAction* B1Plugin::CreatePrimaryGeneratorAction()
 {
-
+    _primaryGeneratorAction.Set(new B1PrimaryGeneratorAction());
+    return _primaryGeneratorAction.Get();
 }
 
 B1EventAction* B1Plugin::GetEventAction()
